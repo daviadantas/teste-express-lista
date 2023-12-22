61d87e2f9f29116feb517582f277546a893a5dae
davi3432 <davi.dantas151@gmail.com>
2023-12-21 22:09:54 -0300

    testando CRUD com lista pra simular banco de dados

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log('servidor iniciado')
+})
+
+const alunos = [];
+
+//buscar todos os usuarios da lista
+app.get('/alunos', (req, res) => {
+    console.log('os usuarios foram buscados')
+    res.status(200).send(alunos)
+})
+
+//buscar usuario por id
+app.get('/alunos/:id', (req, res) => {
+ console.log(req.query)
+ console.log('usuario foi buscado')
+ res.status(200).send(x => x.id == alunos)
+})
+
+//cadastrar um usuario
+app.post('/alunos', (req, res) => {
+ console.log(req.dody)
+ console.log('novo usuario cadastrado')
+ alunos.push(req.body);
+ res.status(200).send(req.body)
+})
+
+app.put('/alunos/:id', (req,res) => {
+    const aluno = alunos.find(x => x.id == req.params.id)
+    const idAluno = alunos.indexOf(aluno)
+    alunos[idAluno] = req.body
+    res.status(200).send('aluno atualizado')
+})  
+
+app.delete('/alunos/:id', (req, res) => {
+    console.log(req.body)
+    const aluno = alunos.find(x => x.id == req.params.id)
+    const idAluno = alunos.indexOf(aluno)
+    alunos.splice(idAluno, 1)
+    res.status(200).send('aluno deletado')
+})
