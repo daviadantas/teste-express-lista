26af5372cae18958c643b38cc03d19c92b42c3ac
davi3432 <davi.dantas151@gmail.com>
2023-12-26 21:23:04 -0300

    api alunos com lista terminado

--- index.js
+++ index.js
@@ -30,17 +30,30 @@ app.post('/alunos', (req, res) => {
  res.status(200).send(req.body)
 })
 
+//atualizar usuario
 app.put('/alunos/:id', (req,res) => {
     const aluno = alunos.find(x => x.id == req.params.id)
     const idAluno = alunos.indexOf(aluno)
-    alunos[idAluno] = req.body
-    res.status(200).send('aluno atualizado')
+    if(idAluno !== -1){
+        alunos[idAluno] = req.body
+        res.status(200).send('aluno atualizado')
+    }
+    else{
+        res.status(404).send('aluno não existe')
+    }
+
 })  
 
+//deletar usuario
 app.delete('/alunos/:id', (req, res) => {
     console.log(req.body)
     const aluno = alunos.find(x => x.id == req.params.id)
     const idAluno = alunos.indexOf(aluno)
-    alunos.splice(idAluno, 1)
-    res.status(200).send('aluno deletado')
+    if(idAluno !== -1){
+        alunos.splice(idAluno, 1)
+        res.status(200).send('aluno deletado')
+    }
+    else{
+        res.status(404).send('aluno não existe')
+    }
 })
